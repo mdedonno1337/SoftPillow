53d71e1edd5d2807bdffd2b4b29887d480ba3bfc
Marco De Donno <Marco.DeDonno@unil.ch>
2016-07-04 00:52:30 +0200

    Rescale the image by a factor passed in argument

--- SoftPillow/__init__.py
+++ SoftPillow/__init__.py
@@ -2,8 +2,25 @@
 # -*- coding: UTF-8 -*-
 
 from PIL import Image
+from Image import NEAREST
 
 class MyImage( Image.Image ):
-    pass
+    def scale( self, factor, resample = NEAREST ):
+        """
+            Returns a rescaled image by a specific factor given in parameter. A
+            factor greater than 1 expands the image, between 0 and 1 contracts
+            the image.
+        """
+        self.load()
+        
+        if factor == 1:
+            return self._new( self.im )
+        
+        elif factor <= 0:
+            raise ValueError( "the factor must be greater than 0" )
+        
+        else:
+            size = ( int( round( factor * self.width ) ), int( round( factor * self.height ) ) )
+            return self.resize( size, resample )
     
 Image.Image = MyImage
